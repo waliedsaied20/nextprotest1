e1e1b37637ac9c8735aec80778efda16b5ce9c61
waliedsaied20 <walied.saied12@yahoo.com>
2023-08-30 17:25:35 +0300

    eidt page with rightbranch

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,20 @@ import Link from "next/link";
 // Edit left Branch
 function HomePage() {
   return (
+<<<<<<< HEAD
     // Edit page with Leftbranch
     <>
    <Head>
     <title>Edit Page with Left branch</title>
           <title>Welcome ! render 1 </title>
+=======
+    // Edit wit rightbranch
+    <>
+   <Head>
+    
+    <title>this box with branch rightbranch</title>
+          <title>Welcome ! render</title>
+>>>>>>> 50a158f (eidt page with rightbranch)
           <link rel="icon" type="image/png" href="homeicon.png"/>
       </Head>
       <div>
